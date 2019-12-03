06e524197caac4490d05e57381e5b2744e68bb45
MorganHuegel <mhue2910@gmail.com>
2019-12-02 22:41:06 -0500

    updated fetch config to Prod database

--- src/fetchFunctions/config.js
+++ src/fetchFunctions/config.js
@@ -3,9 +3,9 @@
 // doesn't support??
 //const { SERVER_BASE_URL } = require('react-native-dotenv')
 
-// const SERVER_BASE_URL = 'https://owen-wilsnake-server.herokuapp.com'
+const SERVER_BASE_URL = 'https://owen-wilsnake-server.herokuapp.com'
 // const SERVER_BASE_URL = 'http://192.168.0.4:8080'
-const SERVER_BASE_URL = 'http://localhost:8080'
+// const SERVER_BASE_URL = 'http://localhost:8080'
 
 module.exports = {
   SERVER_BASE_URL: SERVER_BASE_URL || 'http://192.168.0.4:8080'
